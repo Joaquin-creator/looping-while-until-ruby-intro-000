9653607a7f7cb4d0a6659ebf85d745d8b016e613
Joaquin Esquilin <esquilin.joaquin@gmail.com>
2020-01-14 01:23:43 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
   levitation_force = 6
-  #your code here
+  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	  expect{ using_until }.to output(looping_string).to_stdout
     
 end
 
